e12f9fb38956c77a485ef0a4187d0d898ff8ef99
dot <chodanun@9-140.kuwin.ku.ac.th>
2014-09-21 15:12:55 +0700

    has bg running and boy can run

---
+++ src/RunningDiedGame/BoyDied.java
@@ -0,0 +1,61 @@
+package RunningDiedGame;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public class BoyDied  {
+
+	private Image image ;
+	private float x ;
+	private float y ;
+	private int delaytime = 0 ;
+	private String action = "res/boy.png" ;
+	
+	public BoyDied(float x, float y) throws SlickException{
+		image = new Image(action);
+		this.x = x ;
+		this.y = y ;
+	}
+	
+	public void render () throws SlickException{
+		image.draw(x,y);
+		changeAction();
+	}
+	public void changeAction () throws SlickException{
+		if ( delaytime > 10 ){
+			image = new Image(action);
+			if ( action == "res/boy.png"){ 
+				action = "res/boy2.png";
+			}
+			else {
+				action = "res/boy.png";
+			}
+			delaytime = 0 ;
+		}
+		delaytime++;
+	}
+	
+	public void moveRight (){
+		if ( x == 70 ){
+			x = 440;
+		}
+		else {
+			x = 750 ;
+		}
+	}
+	
+	public void moveLeft (){
+		if ( x == 750){
+			x = 440 ;
+		}
+		else {
+			x = 70 ;
+		}
+	}
+	
+}
--- src/RunningDiedGame/RunningDied.java
+++ src/RunningDiedGame/RunningDied.java
@@ -1,45 +1,57 @@
+package RunningDiedGame;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-
-import bulletgame.BulletGame;
-
 
 public class RunningDied extends BasicGame {
 
+	BoyDied boy ;
+	Image image ;
+	
 	public RunningDied(String title){
 		super(title);
-		// TODO Auto-generated constructor stub
 	}
-
+	
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
-		
+		image.draw(0,-5);
+		boy.render();
 	}
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
-		
+	public void init(GameContainer arg) throws SlickException {
+		image = new Image("res/background.png");
+		boy = new BoyDied(440,440);
 	}
 
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
 		
 	}
 	
+	@Override
+	public void keyPressed (int key, char c) {
+		if (key == Input.KEY_RIGHT) {
+			boy.moveRight();
+	    }
+		else if (key == Input.KEY_LEFT){
+			boy.moveLeft();
+		}
+		
+	}
 	public static void main (String[] args){
 		 try {
 		    	RunningDied game = new RunningDied("Running Too Died Guys GG!!");
 		      	AppGameContainer appgc = new AppGameContainer(game);
-		      	appgc.setDisplayMode(640, 480, false);
-		      	appgc.setMinimumLogicUpdateInterval(1000 / 60);
+		      	appgc.setDisplayMode(1024,650,false);
+		      	appgc.setTargetFrameRate(100);
 		      	appgc.start();
-
 		    } catch (SlickException e) {
 		    	  e.printStackTrace();
 		    }
